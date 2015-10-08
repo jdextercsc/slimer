14cda424f161fc85428c67376b61b62e4df64b7d
Jeff Dexter <jeff.dexter@servicemesh.com>
2015-10-08 09:13:44 -0400

    fixed spelling, and pep8 styling issues

--- library/iptables.py
+++ library/iptables.py
@@ -5,7 +5,8 @@ __author__ = 'jdextercsc'
 DOCUMENTATION = '''
 ---
 module: iptables
-short_description: appends iptables rules to a run time configuration and saves the resulting iptables rule chain to disk
+short_description: appends iptables rules to a run time configuration and saves
+the resulting iptables rule chain to disk
 options:
     protocol:
         required: false
@@ -19,10 +20,12 @@ options:
         description: a list of ports to open
     source:
         required: false
-        description: the IP address to filter for source of packets in form of CIDR notation XX.XX.XX.XX/YY
+        description: the IP address to filter for source of packets in form of
+        CIDR notation XX.XX.XX.XX/YY
     dest:
         required: false
-        description: the IP address to filter for destination of packets in form of CIDR notation XX.XX.XX.XX/YY
+        description: the IP address to filter for destination of packets in
+        form of CIDR notation XX.XX.XX.XX/YY
     comment:
         required: false
         description: a string containing a descriptive comment about the rule.
@@ -32,39 +35,40 @@ options:
 
 '''
 
+
 def main():
     module = AnsibleModule(
-        argument_spec = dict(
+        argument_spec=dict(
             # state     = dict(required=true, choices=['absent', 'present']),
-            protocol  = dict(required=False, default='tcp', choices=['tcp', 'udp']),
-            port      = dict(required=True),
-            source    = dict(required=False),
-            dest      = dict(required=False),
-            comment   = dict(required=False),
-            chain     = dict(default='INPUT', required=False),
+            protocol=dict(required=False, default='tcp', choices=['tcp', 'udp']),
+            port=dict(required=True),
+            source=dict(required=False),
+            dest=dict(required=False),
+            comment=dict(required=False),
+            chain=dict(default='INPUT', required=False),
         ),
         supports_check_mode=True,
     )
     ports = module.params['port']
     if isinstance(ports, list):
         ports = ",".join(map(str, ports))
-    	cmd_string = "%(chain)s" % module.params
-        cmd_string +=" -p %(protocol)s" % module.params
-        cmd_string +=" -m multiport --dport %s" % ports
+        cmd_string = "%(chain)s" % module.params
+        cmd_string += " -p %(protocol)s" % module.params
+        cmd_string += " -m multiport --dport %s" % ports
     else:
-        cmd_string = "%(chain)s -p %(protocol)s --dport %(port)s" %  module.params
-    if module.params.has_key('source') and bool(module.params['source']):
-        cmd_string += " -s %(source)s" %  module.params
-    if module.params.has_key('dest') and bool(module.params['dest']):
-        cmd_string += " -d %(dest)s" %  module.params
-    if module.params.has_key('comment') and bool(module.params['comment']):
-        cmd_string += ' -m comment --comment " %(comment)s"' %  module.params
-    
-    #check if rule exsits
+        cmd_string = "%(chain)s -p %(protocol)s --dport %(port)s" % module.params
+    if 'source' in module.params and bool(module.params['source']):
+        cmd_string += " -s %(source)s" % module.params
+    if 'dest' in module.params and bool(module.params['dest']):
+        cmd_string += " -d %(dest)s" % module.params
+    if 'comment' in module.params and bool(module.params['comment']):
+        cmd_string += ' -m comment --comment " %(comment)s"' % module.params
+
+    # check if rule exists
     cmd = "iptables -vC %s -j ACCEPT" % cmd_string
     rc, out, err = module.run_command(cmd)
     if rc is 0:
-        module.exit_json(changed=False, msg="rule %s already exists." % cmd_string )
+        module.exit_json(changed=False, msg="rule %s already exists." % cmd_string)
     elif rc is 2:
         module.fail_json(msg=err)
     elif rc is 1 and module.check_mode:
@@ -74,11 +78,11 @@ def main():
     cmd = "iptables -vI %s -j ACCEPT" % cmd_string
     rc, out, err = module.run_command(cmd)
     if rc is 1:
-    	module.fail_json(msg="failed to add rule %s with error %s" % (cmd_string,err))
+        module.fail_json(msg="failed to add rule %s with error %s" % (cmd_string, err))
     else:
         cmd = 'service iptables save'
         rc, out, err = module.run_command(cmd)
-        
+
         if rc is 1:
             module.fail_json(msg=err)
         else:
@@ -86,5 +90,6 @@ def main():
 
 
 from ansible.module_utils.basic import *
+
 if __name__ == '__main__':
     main()
